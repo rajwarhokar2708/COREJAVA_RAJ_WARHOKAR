38b43e9cb3915932f3c88fc16791d5c894bd0fc2
rajwarhokar2708 <warhokarraj2708@gamil.com>
2023-11-27 14:55:34 +0530

    Raj 2 file added

---
+++ src/com/mkpits/variable_scope/Raj_2.java
@@ -0,0 +1,11 @@
+package com.mkpits.variable_scope;
+
+public class Raj_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Chall mere bhai");
+
+	}
+
+}
