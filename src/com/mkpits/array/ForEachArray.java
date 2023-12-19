4855583a3d191c9b8854a9224b9d8127178dae29
rajwarhokar2708 <warhokarraj2708@gamil.com>
2023-12-19 15:21:06 +0530

    File Added

---
+++ src/com/mkpits/array/ForEachArray.java
@@ -0,0 +1,10 @@
+package com.mkpits.array;
+
+public class ForEachArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/mkpits/game/SnakeGame.java
@@ -0,0 +1,21 @@
+package com.mkpits.game;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class SnakeGame {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		SnakeGame sg=new SnakeGame();
+		sg.sGame();
+
+	}
+
+	public void sGame() throws NumberFormatException, IOException {
+		//BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
